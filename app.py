41795b3024377266c61c7e16f9a9c90a7cdf0105
maharaha <129414120+maharaha@users.noreply.github.com>
2025-05-20 23:36:07 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from lessons import lessons
-from check_code import check_user_code
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", lessons=lessons)
+    return render_template("index.html")
 
-@app.route("/lesson/<int:lesson_id>", methods=["GET", "POST"])
+@app.route("/structure")
+def structure():
+    return render_template("structure.html", lessons=lessons)
+
+@app.route("/lesson/<int:lesson_id>")
 def lesson(lesson_id):
-    if lesson_id < 0 or lesson_id >= len(lessons):
-        return "Урок не найден", 404
-
     lesson = lessons[lesson_id]
-    result = ""
-    if request.method == "POST":
-        code = request.form.get("code", "")
-        result = check_user_code(code)
-    return render_template("lesson.html", lesson=lesson, lesson_id=lesson_id, result=result)
+    return render_template("lesson.html", lesson=lesson, lesson_id=lesson_id)
 
 if __name__ == "__main__":
     app.run()
